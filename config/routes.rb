832f15dd9c8405959376fbb310fdae140c19d444
escowin <edwin@escowinart.com>
2022-12-13 08:50:45 -0600

    8.4 | commenting functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "articles#index" # root maps to index
 
   # CRUD entity
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
