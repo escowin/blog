304716324829b389e452dfb16310f6c84d5f654e
escowin <edwin@escowinart.com>
2022-12-12 22:56:33 -0600

    7.3.4 | CRud built

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index" # root maps to index
 
-  get "/articles", to: "articles#index" # GET route maps to ArticlesController index action
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # CRUD entity
+  resources :articles
 end
