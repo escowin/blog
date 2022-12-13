955bfa71e7ce6402b5e943c4c9ee006f0ffac594
escowin <edwin@escowinart.com>
2022-12-12 23:27:45 -0600

    7.4.2 | CRUd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,40 @@
 class ArticlesController < ApplicationController
-  def index
+  # CRUD | retrieve
+  def index # get articles
     @articles = Article.all
   end
 
-  def show
+  def show # get an article
     @article = Article.find(params[:id])
   end
 
-  def new
+  # CRUD | create
+  def new # post an article
     @article = Article.new
   end
 
-  def create
+  def create # post cont.
     @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  # CRUD | update
+  def edit # fetches article from db, stores it in @article for form building. by default, renders .../edit.html.rb
+    @article = Article.find(params[:id])
+  end
+
+  def update # refetches article fom db, updates it with submitted form data.
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
